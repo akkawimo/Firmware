9cf06361386dd78f39d018b40a875a35c0bda2b5
Alessandro Simovic <alessandro@auterion.com>
2022-01-06 12:32:57 +0100

    moved precland from navigator to flighttasks
    PrecisionLanding flight task is not being compiled yet. See next commit.

--- src/modules/navigator/navigator.h
+++ src/modules/navigator/navigator.h
@@ -43,7 +43,6 @@
 
 #include "geofence.h"
 #include "land.h"
-#include "precland.h"
 #include "loiter.h"
 #include "mission.h"
 #include "navigator_mode.h"
@@ -158,8 +157,6 @@ public:
 	vehicle_land_detected_s     *get_land_detected() { return &_land_detected; }
 	vehicle_local_position_s    *get_local_position() { return &_local_pos; }
 	vehicle_status_s            *get_vstatus() { return &_vstatus; }
-
-	PrecLand *get_precland() { return &_precland; } /**< allow others, e.g. Mission, to use the precision land block */
 
 	const vehicle_roi_s &get_vroi() { return _vroi; }
 
@@ -372,7 +369,6 @@ private:
 	Takeoff		_takeoff;			/**< class for handling takeoff commands */
 	VtolTakeoff	_vtol_takeoff;			/**< class for handling VEHICLE_CMD_NAV_VTOL_TAKEOFF command */
 	Land		_land;			/**< class for handling land commands */
-	PrecLand	_precland;			/**< class for handling precision land commands */
 	RTL 		_rtl;				/**< class that handles RTL */
 	AdsbConflict 	_adsb_conflict;			/**< class that handles ADSB conflict avoidance */
 
